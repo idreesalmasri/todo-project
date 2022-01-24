12ab421a6267f447ac9562625589833c441a9887
idreesalmasri <idrees.massri95@gmail.com>
2022-01-24 07:50:47 +0200

    task#6

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
 console.log("iam alive;")
-var firstName= prompt ("insert your name");
-var g =prompt ("your gender is : male or female");
-var age = prompt("insert your age");
-if (age <=0){
-    alert ("wrong age input");
+// var firstName= prompt ("insert your name");
+
+// var g =prompt ("your gender is : male or female");
+// var age = prompt("insert your age");
+// if (age <=0){
+//     alert ("wrong age input");
+// }
+// // var welcoming = confirm ("do you want to skip welcoming message");
+// if(g.toLowerCase() === "male"){
+//     var m ="Mr";
+// }
+// else if (g.toLowerCase() ==="female"){
+//     var m="Ms";
+// }
+// else {
+//     var m="";
+// }
+// if (!welcoming){
+//     var message = "welcome " +m +" "+ firstName ;
+    // alert (message);
+// }
+alert("In the following questions, answer yes or no in lowercase");
+let answer1 ="";
+let answer2 ="";
+let answer3 ="";
+answer1=prompt("are you doing your tasks?");
+answer2=prompt("Do you face difficulty in it?");
+answer3=prompt("are the task boring?");
+if(answer1==""||answer1==null){
+    answer1="invalid";
 }
-var welcoming = confirm ("do you want to skip welcoming message");
-if(g.toLowerCase() === "male"){
-    var m ="Mr";
+if(answer2==""||answer2==null){
+    answer2="invalid";
 }
-else if (g.toLowerCase() ==="female"){
-    var m="Ms";
+if(answer3==""||answer3==null){
+    answer3="invalid";
 }
-else {
-    var m="";
-}
-if (!welcoming){
-    var message = "welcome " +m +" "+ firstName ;
-    alert (message);
+let answeraArray=[answer1,answer2,answer3];
+for(i=0;i<answeraArray.length;i++){
+    console.log(answeraArray[i]);
 }

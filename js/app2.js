d989f6b56f694b35acc4123de8c591d991807601
idreesalmasri <idrees.massri95@gmail.com>
2022-01-24 14:30:17 +0200

    try2

--- js/app2.js
+++ js/app2.js
@@ -1,19 +1,20 @@
-alert("In the following questions, answer yes or no in lowercase");
-let answer1 ="";
-let answer2 ="";
-let answer3 ="";
-answer1=prompt("are you doing your tasks?");
-answer2=prompt("Do you face difficulty in it?");
-answer3=prompt("are the task boring?");
-if(answer1==""||answer1==null){
-    answer1="invalid";
+
+function askTheUser(question){
+    let answer = prompt(question);
+    if(answer==""||answer==null){
+        return answer="invalid";
+    }
+    else if(answer=="yes"||answer=="no"){
+        return answer;
+    }
+    else{
+        alert("refrish and insert yes or no")
+
+    }
 }
-if(answer2==""||answer2==null){
-    answer2="invalid";
-}
-if(answer3==""||answer3==null){
-    answer3="invalid";
-}
+let answer1=askTheUser("are you doing your tasks?");
+let answer2=askTheUser("Do you face difficulty in it?");
+let answer3=askTheUser("are the task boring?");
 let answeraArray=[answer1,answer2,answer3];
 for(i=0;i<answeraArray.length;i++){
     console.log(answeraArray[i]);
